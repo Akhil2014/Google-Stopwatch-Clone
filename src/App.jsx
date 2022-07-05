e4bee12465308850e00d0541dcb2993ccc86f2fd
Akhil Khan <akhilkhan0786khan@gmail.com>
2022-07-05 17:41:21 +0530

    Working on Mounting and Unmounting Phase

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,32 @@
+import { useState } from "react";
 import { BiStopwatch } from "react-icons/bi";
 import { GiSandsOfTime } from "react-icons/gi";
 import './App.css';
+import StopWatch from "./Components/StopWatch";
+import Timer from "./Components/Timer";
 
 function App() {
+  const [mount,setMount] = useState(true)
+  const handleMount =(e) => {
+    if(e===1){
+      setMount(false)
+    }else if(e==2){
+      setMount(true)
+    }
+  }
   return (
     <div className="App">
      <div className='button-div'>
       <div>
-      <button className='button'><GiSandsOfTime className="icon"/>TIMER</button>
+      <button onClick={() => handleMount(1)} className='button button2'><GiSandsOfTime className="icon"/>TIMER</button>
       </div>
       <div>
-      <button className='button'><BiStopwatch className="icon" />STOPWATCH</button>
+      <button onClick={() => handleMount(2)} className='button button1'><BiStopwatch className="icon" />STOPWATCH</button>
       </div>
        </div>
 
        <div className="mount">
-        Mount
+       { (mount)?<StopWatch />:<Timer />}
        </div>
 
        <div>
